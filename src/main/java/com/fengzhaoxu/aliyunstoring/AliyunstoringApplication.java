b528dcc05ea4d7fadfadb11b9cb1e8dde34af4f3
fengzhaoxu <1965813966@qq.com>
2021-10-28 01:56:47 +0800

    新建SpringBoot项目,添加aliyunoss依赖。

---
+++ src/main/java/com/fengzhaoxu/aliyunstoring/AliyunstoringApplication.java
@@ -0,0 +1,13 @@
+package com.fengzhaoxu.aliyunstoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliyunstoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliyunstoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengzhaoxu/aliyunstoring/AliyunstoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengzhaoxu.aliyunstoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliyunstoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
